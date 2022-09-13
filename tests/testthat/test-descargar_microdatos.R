f5442a9a165653f6fb9cf76f7e510ef120b0b388
pablotis <pablotisco@gmail.com>
2022-09-13 14:09:04 -0300

    Cargo test para descargar_microdatos()

---
+++ tests/testthat/test-descargar_microdatos.R
@@ -0,0 +1,11 @@
+test_that("Descarga de EVyTH", {
+  skip_if_offline()
+  expect_error(base <- descargar_microdatos(anio = 2011, trimestre = 2, variables =  c('anio','tipo_visitante','pondera')))
+  base <- descargar_microdatos(anio = 2019, trimestre = 2, variables =  c('anio', 'trimestre', 'tipo_visitante','pondera'))
+  expect_equal(unique(base$anio), 2019)
+  expect_equal(unique(base$trimestre), 2)
+  variables <- names(base)
+  expect_equal(variables, c("anio", "trimestre", "tipo_visitante", "pondera"))
+  dimensiones <- dim(base)
+  expect_equal(dimensiones, c(8135,4))
+})
