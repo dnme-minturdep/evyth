be8ca0429967cd78332e73b6994b4f0ec028ae0e
pablotis <pablotisco@gmail.com>
2022-09-19 22:11:17 -0300

    Add NEWS.md

---
+++ NEWS.md
@@ -0,0 +1,8 @@
+# evyth 0.0.0.9001
+
+* Se agrega función `escribir_base_microdato()`
+* Se mejora funcionamiento de `crear_base_trabajo()` y `crear_base_microdato()`
+
+# evyth 0.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
---
+++ R/escribo_base.R
@@ -0,0 +1,53 @@
+#' Title
+#'
+#' @param formato
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+
+
+escribo_base <- function(archivo = NULL, formato = "csv"){
+
+  if(formato == "cvs"){
+    ### Armo base usuaria en formato .csv
+    readr::write_csv(x = archivo,
+                     file = "/srv/DataDNMYE/evyth/microdatos/evyth_microdatos.csv", na = "",
+                     progress = FALSE)
+  }
+
+  if(formato == "txt"){
+
+
+    ### Armo base usuaria en formato .txt
+    readr::write_delim(x = archivo,
+                       file = "/srv/DataDNMYE/evyth/microdatos/evyth_microdatos.txt",
+                       delim = ",", na = "",
+                       progress = FALSE)
+  }
+
+  if(formato == "sav"){
+
+    ### Armo base usuaria en formato .sav (spss)
+    haven::write_sav(data = archivo,
+                     path = "/srv/DataDNMYE/evyth/microdatos/evyth_microdatos.sav")
+
+  }
+
+  if(formato == "dta"){
+
+    ### Armo base usuaria en formato .dta (stata)
+    haven::write_dta(data = archivo,
+                     path = "/srv/DataDNMYE/evyth/microdatos/evyth_microdatos.dta")
+  }
+
+  if(formato == "xlsx"){
+
+    ### Armo base usuaria en formato .xlsx (Excel)
+    writexl::write_xlsx(x = archivo,
+                        path = "/srv/DataDNMYE/evyth/microdatos/evyth_microdatos.xlsx")
+  }
+}
