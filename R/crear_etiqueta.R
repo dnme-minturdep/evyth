e6a82da09a6aa24507802a5d54d6383ca9779ab6
pablotis <pablotisco@gmail.com>
2022-10-11 17:55:01 -0300

    actualizo base diccionario desde data/

--- R/crear_etiqueta.R
+++ R/crear_etiqueta.R
@@ -13,7 +13,6 @@
 
 crear_etiqueta <- function(base, variables = NULL, drop_vars = T) {
 
-  #diccionario <- data.table::fread("http://datos.yvera.gob.ar/dataset/b5819e9b-5edf-4aad-bd39-a81158a2b3f3/resource/d8107236-f5ba-428d-8a31-f9b2034d8c8f/download/evyth_diccionario_registro.csv")
   diccionario <- evyth::diccionario
 
 
